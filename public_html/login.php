ea8897474f59d4d2da9a554ca85862d38c065665
www-data <www-data@fetcher.cdacos.com>
2018-05-09 21:38:33 +0100

    Improved login layout

--- public_html/login.php
+++ public_html/login.php
@@ -6,10 +6,20 @@
   }
 ?>
 <?php include 'html_header.php'; ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    .container {
+      width: 50%;
+      margin: 0 auto;
+    }
+    .container form div {
+      margin-bottom: 20px;
+    }
+  </style>
   <div class="container">
     <h3 class="text-center">Login</h3>
     <?php
-      if(isset($_POST['submit'])){
+      if(isset($_POST['password'])){
         $password = $_POST['password'];
         $config = parse_ini_file(__DIR__ . '/../config.ini', true);
         $hash = $config['notes']['password_hash'];
@@ -26,9 +36,9 @@
     <form action="" method="post">
       <div class="form-group">
         <label for="pwd">Password:</label>
-        <input type="password" class="form-control" id="pwd" name="password" required>
+        <input type="password" id="pwd" name="password" required>
       </div>
-      <button type="submit" name="submit" class="btn btn-default">Login</button>
+      <button type="submit">Login</button>
     </form>
   </div>
 <?php include 'html_footer.php'; ?>
